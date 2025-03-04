fea55426bdd24e2e99ae629de9246dbc47c740e8
Masum Billah <masumbillah2062003@gmail.com>
2025-03-04 23:50:23 +0600

    added new data and updated data

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -4,17 +4,35 @@ import express from 'express';
 import { StudentController } from './student.controller';
 import { auth } from '../../../middleware/auth';
 import { USER_ROLE } from '../user/user.contatnt';
+import validationRequest from '../../../middleware/validationRequest';
+import { updateStudentValidationSchema } from './student.zod.validation';
 
 const router = express.Router();
 
 // we call controller funciton
 router.get(
   '/',
-  auth(USER_ROLE.admin, USER_ROLE.student),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   StudentController.getAllStudents,
 );
-router.get('/:id',auth(USER_ROLE.admin, USER_ROLE.faculty), StudentController.getSingleStudent);
-router.delete('/:id', StudentController.deleteStudent);
-router.patch('/:id', StudentController.updateStudent);
+
+router.get(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin, USER_ROLE.faculty),
+  StudentController.getSingleStudent,
+);
+
+router.patch(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validationRequest(updateStudentValidationSchema),
+  StudentController.updateStudent,
+);
+
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  StudentController.deleteStudent,
+);
 
 export const StudentRoute = router;
--- src/app/modules/student/student.zod.validation.ts
+++ src/app/modules/student/student.zod.validation.ts
@@ -1,216 +1,99 @@
 import { z } from 'zod';
 
-// Zod schema for UserName
-const userNameZodSchema = z.object({
+const createUserNameValidationSchema = z.object({
   firstName: z
     .string()
-    .min(1, { message: 'First name is required.' }) // Replacing .nonempty() with .min(1)
-    .max(20, { message: 'Name cannot be more than 20 characters.' })
-    .refine(
-      (value) => value.charAt(0).toUpperCase() + value.slice(1) === value,
-      {
-        message: 'First name must be in capitalized format.',
-      },
-    ),
-  middleName: z.string().optional(),
-  lastName: z
-    .string()
-    .min(1, { message: 'Last name is required.' }) // Replacing .nonempty() with .min(1)
-    .refine((value) => /^[A-Za-z]+$/.test(value), {
-      message: 'Last name must only contain alphabetic characters.',
+    .min(1)
+    .max(20)
+    .refine((value) => /^[A-Z]/.test(value), {
+      message: 'First Name must start with a capital letter',
     }),
+  middleName: z.string(),
+  lastName: z.string(),
 });
 
-// Zod schema for Guardian
-const guardianZodSchema = z.object({
-  fatherName: z.string().min(1, { message: 'Father name is required.' }),
-  fatherOccupation: z
-    .string()
-    .min(1, { message: 'Father occupation is required.' }),
-  fatherContactNo: z
-    .string()
-    .min(1, { message: 'Father contact number is required.' }),
-  motherName: z.string().min(1, { message: 'Mother name is required.' }),
-  motherOccupation: z
-    .string()
-    .min(1, { message: 'Mother occupation is required.' }),
-  motherContactNo: z
-    .string()
-    .min(1, { message: 'Mother contact number is required.' }),
+const createGuardianValidationSchema = z.object({
+  fatherName: z.string(),
+  fatherOccupation: z.string(),
+  fatherContactNo: z.string(),
+  motherName: z.string(),
+  motherOccupation: z.string(),
+  motherContactNo: z.string(),
 });
 
-// Zod schema for LocalGuardian
-const localGuardianZodSchema = z.object({
-  name: z.string().min(1, { message: 'Local guardian name is required.' }),
-  contact: z
-    .string()
-    .min(1, { message: 'Local guardian contact is required.' }),
-  occupation: z
-    .string()
-    .min(1, { message: 'Local guardian occupation is required.' }),
-  address: z
-    .string()
-    .min(1, { message: 'Local guardian address is required.' }),
+const createLocalGuardianValidationSchema = z.object({
+  name: z.string(),
+  occupation: z.string(),
+  contactNo: z.string(),
+  address: z.string(),
 });
 
-// Zod schema for Student
-export const createStudentZodSchema = z.object({
-  body: z.object({
-    password: z.string().max(20),
-    student: z.object({
-      name: userNameZodSchema,
-      gender: z.enum(['male', 'female', 'other'], {
-        errorMap: () => ({
-          message: 'Gender must be either male, female, or other.',
-        }),
-      }),
-      dateOfBirth: z.string().optional(),
-      email: z
-        .string()
-        .min(1, { message: 'Email is required.' }) // Replacing .nonempty() with .min(1)
-        .email({ message: 'Invalid email format.' }),
-      contactNo: z.string().min(1, { message: 'Contact number is required.' }),
-      emergencyContactNo: z
-        .string()
-        .min(1, { message: 'Emergency contact number is required.' }),
-      bloodGroup: z
-        .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], {
-          errorMap: () => ({
-            message:
-              'Blood group must be one of A+, A-, B+, B-, AB+, AB-, O+, O-.',
-          }),
-        })
-        .optional(),
-      presentAddress: z.string(),
-      permanentAddress: z.string(),
-      guardian: guardianZodSchema,
-      localGuardian: localGuardianZodSchema,
-      academicDepartment: z.string(),
-      admissionSemester: z.string(),
-      profileImg: z.string().optional(),
-    }),
-  }),
-});
-
-const updateUserNameZodSchema = z.object({
-  firstName: z
-    .string()
-    .min(1, { message: 'First name is required.' })
-    .max(20, { message: 'Name cannot be more than 20 characters.' })
-    .refine(
-      (value) => value.charAt(0).toUpperCase() + value.slice(1) === value,
-      {
-        message: 'First name must be in capitalized format.',
-      },
-    )
-    .optional(),
-  middleName: z.string().optional(),
-  lastName: z
-    .string()
-    .min(1, { message: 'Last name is required.' })
-    .refine((value) => /^[A-Za-z]+$/.test(value), {
-      message: 'Last name must only contain alphabetic characters.',
-    })
-    .optional(),
-});
-
-// Zod schema for Guardian
-const UpdateGuardianZodSchema = z.object({
-  fatherName: z
-    .string()
-    .min(1, { message: 'Father name is required.' })
-    .optional(),
-  fatherOccupation: z
-    .string()
-    .min(1, { message: 'Father occupation is required.' })
-    .optional(),
-  fatherContactNo: z
-    .string()
-    .min(1, { message: 'Father contact number is required.' })
-    .optional(),
-  motherName: z
-    .string()
-    .min(1, { message: 'Mother name is required.' })
-    .optional(),
-  motherOccupation: z
-    .string()
-    .min(1, { message: 'Mother occupation is required.' })
-    .optional(),
-  motherContactNo: z
-    .string()
-    .min(1, { message: 'Mother contact number is required.' })
-    .optional(),
-});
-
-// Zod schema for LocalGuardian
-const updateLocalGuardianZodSchema = z.object({
-  name: z
-    .string()
-    .min(1, { message: 'Local guardian name is required.' })
-    .optional(),
-  contact: z
-    .string()
-    .min(1, { message: 'Local guardian contact is required.' })
-    .optional(),
-  occupation: z
-    .string()
-    .min(1, { message: 'Local guardian occupation is required.' })
-    .optional(),
-  address: z
-    .string()
-    .min(1, { message: 'Local guardian address is required.' })
-    .optional(),
-});
-
-// Zod schema for Student
-export const updateStudentZodSchema = z.object({
+export const createStudentValidationSchema = z.object({
   body: z.object({
     password: z.string().max(20).optional(),
-    student: z
-      .object({
-        name: updateUserNameZodSchema.optional(),
-        gender: z
-          .enum(['male', 'female', 'other'], {
-            errorMap: () => ({
-              message: 'Gender must be either male, female, or other.',
-            }),
-          })
-          .optional(),
-        dateOfBirth: z.string().optional(),
-        email: z
-          .string()
-          .min(1, { message: 'Email is required.' })
-          .email({ message: 'Invalid email format.' })
-          .optional(),
-        contactNo: z
-          .string()
-          .min(1, { message: 'Contact number is required.' })
-          .optional(),
-        emergencyContactNo: z
-          .string()
-          .min(1, { message: 'Emergency contact number is required.' })
-          .optional(),
-        bloodGroup: z
-          .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], {
-            errorMap: () => ({
-              message:
-                'Blood group must be one of A+, A-, B+, B-, AB+, AB-, O+, O-.',
-            }),
-          })
-          .optional(),
-        presentAddress: z.string().optional(),
-        permanentAddress: z.string().optional(),
-        guardian: UpdateGuardianZodSchema.optional(),
-        localGuardian: updateLocalGuardianZodSchema.optional(),
-        academicDepartment: z.string().optional(),
-        admissionSemester: z.string().optional(),
-        profileImg: z.string().optional(),
-      })
-      .optional(),
+    student: z.object({
+      name: createUserNameValidationSchema,
+      gender: z.enum(['male', 'female', 'other']),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email(),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      bloogGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
+      guardian: createGuardianValidationSchema,
+      localGuardian: createLocalGuardianValidationSchema,
+      admissionSemester: z.string(),
+      academicDepartment: z.string(),
+    }),
   }),
 });
 
-export const studentValidationZod = {
-  createStudentZodSchema,
-  updateStudentZodSchema,
+const updateUserNameValidationSchema = z.object({
+  firstName: z.string().min(1).max(20).optional(),
+  middleName: z.string().optional(),
+  lastName: z.string().optional(),
+});
+
+const updateGuardianValidationSchema = z.object({
+  fatherName: z.string().optional(),
+  fatherOccupation: z.string().optional(),
+  fatherContactNo: z.string().optional(),
+  motherName: z.string().optional(),
+  motherOccupation: z.string().optional(),
+  motherContactNo: z.string().optional(),
+});
+
+const updateLocalGuardianValidationSchema = z.object({
+  name: z.string().optional(),
+  occupation: z.string().optional(),
+  contactNo: z.string().optional(),
+  address: z.string().optional(),
+});
+
+export const updateStudentValidationSchema = z.object({
+  body: z.object({
+    student: z.object({
+      name: updateUserNameValidationSchema,
+      gender: z.enum(['male', 'female', 'other']).optional(),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email().optional(),
+      contactNo: z.string().optional(),
+      emergencyContactNo: z.string().optional(),
+      bloogGroup: z
+        .enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'])
+        .optional(),
+      presentAddress: z.string().optional(),
+      permanentAddress: z.string().optional(),
+      guardian: updateGuardianValidationSchema.optional(),
+      localGuardian: updateLocalGuardianValidationSchema.optional(),
+      admissionSemester: z.string().optional(),
+      academicDepartment: z.string().optional(),
+    }),
+  }),
+});
+
+export const studentValidations = {
+  createStudentValidationSchema,
+  updateStudentValidationSchema,
 };
