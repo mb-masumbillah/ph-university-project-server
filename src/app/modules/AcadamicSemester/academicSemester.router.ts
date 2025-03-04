1e2be0e76969c82fd0f03a81a5823eb4a3ad25ea
Masum Billah <masumbillah2062003@gmail.com>
2025-03-04 23:40:54 +0600

    added new data and updated data

--- src/app/modules/AcadamicSemester/academicSemester.router.ts
+++ src/app/modules/AcadamicSemester/academicSemester.router.ts
@@ -2,20 +2,45 @@ import express from 'express';
 import { academicSemesterControllers } from './academicSemester.controller';
 import { AcademicSemesterValidations } from './academicSemester.zod.validation';
 import validationRequest from '../../../middleware/validationRequest';
+import { USER_ROLE } from '../user/user.contatnt';
+import { auth } from '../../../middleware/auth';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validationRequest(AcademicSemesterValidations.CreateAcademicValidationSchema),
   academicSemesterControllers.createAcademicSemester,
 );
-router.get('/', academicSemesterControllers.getAllAcademicSemester);
-router.get('/:id', academicSemesterControllers.getSingleAcademicSemester);
+
+router.get(
+  '/:courseId',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  academicSemesterControllers.getSingleAcademicSemester,
+);
+
 router.patch(
-  '/:id',
+  '/:courseId',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validationRequest(AcademicSemesterValidations.UpdateAcademicValidationSchema),
   academicSemesterControllers.updateAcademicSemester,
+);
+
+router.get(
+  '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  academicSemesterControllers.getAllAcademicSemester,
 );
 
 export const academicSemesterRouters = router;
