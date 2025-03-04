da8945de0504db36c733b1687d2ba9b5a3e6de9c
Masum Billah <masumbillah2062003@gmail.com>
2025-03-04 23:41:34 +0600

    added new data and updated data

--- src/app/modules/semesterRegister/semsterRegister.router.ts
+++ src/app/modules/semesterRegister/semsterRegister.router.ts
@@ -2,26 +2,53 @@ import { Router } from 'express';
 import { semesterRegisterController } from './semesterRegister.controller';
 import validationRequest from '../../../middleware/validationRequest';
 import { SemesterRegistrationValidations } from './semesterRegister.validation';
+import { USER_ROLE } from '../user/user.contatnt';
 
 const router = Router();
 
 router.post(
-  '/create-semesterRegister',
-  validationRequest(
+  '/create-semester-registration',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validateRequest(
     SemesterRegistrationValidations.createSemesterRegistrationValidationSchema,
   ),
-  semesterRegisterController.createSemesterRegister,
+  SemesterRegistrationController.createSemesterRegistration,
 );
 
-router.get('/', semesterRegisterController.getAllSemesterRegister);
-router.get('/:id', semesterRegisterController.getSingleSemesterRegister);
+router.get(
+  '/:id',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  SemesterRegistrationController.getSingleSemesterRegistration,
+);
 
 router.patch(
   '/:id',
-  validationRequest(
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validateRequest(
     SemesterRegistrationValidations.upadateSemesterRegistrationValidationSchema,
   ),
-  semesterRegisterController.updateSemesterRegister,
+  SemesterRegistrationController.updateSemesterRegistration,
 );
 
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  SemesterRegistrationController.deleteSemesterRegistration,
+);
+
+router.get(
+  '/',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  SemesterRegistrationController.getAllSemesterRegistrations,
+);
 export const SemesterRegister = router;
