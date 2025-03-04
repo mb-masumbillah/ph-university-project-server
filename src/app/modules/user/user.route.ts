14a6cb5ac199ace436524608228752f41c44b71f
Masum Billah <masumbillah2062003@gmail.com>
2025-03-04 23:51:49 +0600

    added new data and updated data

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,7 +3,7 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { userController } from './user.controller';
 import validationRequest from '../../../middleware/validationRequest';
-import { createStudentZodSchema } from '../student/student.zod.validation';
+import { createStudentValidationSchema, createStudentZodSchema } from '../student/student.zod.validation';
 import { createFacultyValidationSchema } from '../Faculty/factuly.validation';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
 import { auth } from '../../../middleware/auth';
@@ -16,35 +16,57 @@ const router = express.Router();
 // we call controller funciton
 router.post(
   '/create-student',
-  auth(USER_ROLE.admin),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   upload.single('file'),
   (req: Request, res: Response, next: NextFunction) => {
     req.body = JSON.parse(req.body.data);
     next();
   },
-  validationRequest(createStudentZodSchema),
+  validationRequest(createStudentValidationSchema),
   userController.createUser,
 );
+
 router.post(
   '/create-faculty',
-  auth(USER_ROLE?.admin),
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
   validationRequest(createFacultyValidationSchema),
-  userController.createFaculty,
+  UserControllers.createFaculty,
 );
 
 router.post(
   '/create-admin',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = JSON.parse(req.body.data);
+    next();
+  },
   validationRequest(createAdminValidationSchema),
-  userController.createAdmin,
+  UserControllers.createAdmin,
 );
-
-router.get('/me', auth('student', 'faculty', 'admin'), userController.getMe);
 
 router.post(
   '/change-status/:id',
-  auth('admin'),
-  validationRequest(userValidation.changeStatusValidationSchema),
-  userController.changeStatus,
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  validationRequest(UserValidation.changeStatusValidationSchema),
+  UserControllers.changeStatus,
 );
+
+router.get(
+  '/me',
+  auth(
+    USER_ROLE.superAdmin,
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  UserControllers.getMe,
+);
+
 
 export const UserRoute = router;
